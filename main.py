51e4676fef2bb2a1e57762277d1caf4cf7a721a4
Jacek Wielemborek <d33tah@gmail.com>
2020-11-28 14:07:52 +0100

    Uprość kod, dodaj docker-compose, logowanie, failsafe 60s na pocz

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
 #!/usr/bin/env python3.8
 
-import multiprocessing
+import logging
 import time
 
 import requests
 import irc.client
 
 
-def notify_thread(msg):
+def notify(msg):
     client = irc.client.Reactor()
     server = client.server()
-    server.connect('irc.freenode.net', 6667, 'hsldzat')
+    server.connect("irc.freenode.net", 6667, "hsldzat")
 
     def on_connect(connection, event):
-        connection.join('#hakierspejs')
+        connection.join("#hakierspejs")
 
     def on_join(connection, event):
-        connection.privmsg('#hakierspejs', msg)
+        connection.privmsg("#hakierspejs", msg)
         connection.quit()
+        raise RuntimeError()
 
     client.add_global_handler("welcome", on_connect)
     client.add_global_handler("join", on_join)
-    client.process_forever()
-
-
-def notify(msg):
-    t = multiprocessing.Process(target=notify_thread, args=(msg, ))
-    t.start()
-    time.sleep(60)
-    t.terminate()
+    try:
+        client.process_forever()
+    except RuntimeError:
+        pass
 
 
 def isitopen():
     return bool(
         len(
             requests.get(
-                'https://at.hs-ldz.pl/api/v1/users?online=true').json()
+                "https://at.hs-ldz.pl/api/v1/users?online=true"
+            ).json()
         )
     )
 
@@ -43,14 +41,15 @@ def isitopen():
 def main():
     isopen = isitopen()
     while True:
+        time.sleep(600)
         nowisopen = isitopen()
         if nowisopen and not isopen:
-            notify('Spejs jest otwarty!')
+            notify("Spejs jest otwarty! Więcej info: https://at.hs-ldz.pl")
         elif isopen and not nowisopen:
-            notify('Spejs jest zamknięty')
+            notify("Spejs jest zamknięty! Więcej info: https://at.hs-ldz.pl")
         isopen = nowisopen
-        time.sleep(600)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    logging.basicConfig(level='INFO')
     main()
